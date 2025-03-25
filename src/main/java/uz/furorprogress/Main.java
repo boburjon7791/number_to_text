577997ebdc3d76afc55c29d5f6936cd11dd0ca9c
Boburjon <soliyevboburjon95@gmail.com>
2025-03-25 17:15:48 +0500

    finished writing other test cases

--- src/main/java/uz/furorprogress/Main.java
+++ src/main/java/uz/furorprogress/Main.java
@@ -1,8 +1,9 @@
 package uz.furorprogress;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     static long[] testNumbers = {
@@ -11,35 +12,112 @@ public class Main {
             1000000L, 2345678L, 50000000L, 123456789L, 999999999L,
             1000000000L, 5432109876L, 50000000000L, 987654321000L, 999999999999L,
             1000000000000L, 2147483648000L, 5678901234567L, 50000000000000L,
-            99999999999999L, 100000000000000L, 100070001040509L, 940070001040509L,
+            99999999999999L, 100070001040509L, 940070001040509L,
             3, 27, 58, 112, 235, 789, 1543, 6789, 12345, 98765,
-            543210, 876543, 1000001, 3456789, 56789012, 876543210,
+            543210, 876543, 1000001, 3456789, 56789012, 176143110,
             2147483647L, 500008000000L, 9994999999999L, 100000000000000L
     };
     static final Random RANDOM = new Random();
 
+    static DecimalFormat df = new DecimalFormat("0.##");
+    static {
+        df.setMaximumFractionDigits(2); // Kasr qismini cheklash
+    }
+
+    static double[] testDoubleNumbers = Arrays.stream(testNumbers)
+            .mapToDouble(num -> num + Math.random()) // Random kasr qo'shish
+            .filter(num -> num < 1_000_000_000_000_000L) // 15 xonadan katta bo'lmasligi uchun
+            .map(operand -> {
+                double multiplicationResult = operand * 2;
+                return multiplicationResult / 2;
+            })
+            .toArray();
+    static {
+        System.out.println(Arrays.toString(Arrays.stream(testDoubleNumbers).boxed().map(number -> df.format(number)).toArray(String[]::new)));
+    }
+
+
     public static void main(String[] args) {
-        for (long number : testNumbers) {
-            System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uz"));
-            System.out.println();
-            System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "ru"));
-            System.out.println();
-            System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uk"));
-            System.out.println();
-            System.out.println("---------------");
+        testInputs();
+//        testStaticNumbers();
+    }
+
+    static void testInputs(){
+        double number;
+        while (true){
+            try {
+                number = new Scanner(System.in).nextDouble();
+                System.out.printf("%s = %s", df.format(number), NumberToTextUtils.toText(number, "uz"));
+                System.out.println();
+                System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "ru"));
+                System.out.println();
+                System.out.println("---------------");
+                System.out.println();
+                System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "uk"));
+                System.out.println();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
-        for (int i = 0; i < 10; i++) {
-            long number;
-            do{
-                number=RANDOM.nextLong();
-            } while (number>999_999_999_999_999L || number<-999_999_999_999_999L);
-            System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uz"));
-            System.out.println();
-            System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "ru"));
-            System.out.println();
-            System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uk"));
-            System.out.println();
-            System.out.println("---------------");
+    }
+
+    static void testStaticNumbers(){
+        while (true) {
+            try {
+                for (long number : testNumbers) {
+                    System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uz"));
+                    System.out.println();
+                    System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "ru"));
+                    System.out.println();
+                    System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uk"));
+                    System.out.println();
+                    System.out.println("---------------");
+                }
+                for (double number : testDoubleNumbers) {
+                    System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "uz"));
+                    System.out.println();
+                    System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "ru"));
+                    System.out.println();
+                    System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "uk"));
+                    System.out.println();
+                    System.out.println("---------------");
+                }
+                long min = -999_999_999_999_999L;
+                long max = 999_999_999_999_999L;
+                for (int i = 0; i < 10; i++) {
+                    long number = RANDOM.nextLong() % max;
+                    System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uz"));
+                    System.out.println();
+                    System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "ru"));
+                    System.out.println();
+                    System.out.printf("%s = %s", number,  NumberToTextUtils.toText(number, "uk"));
+                    System.out.println();
+                    System.out.println("---------------");
+                }
+                System.out.print("Input testing numbers: ");
+                double number = new Scanner(System.in).nextDouble();
+                System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "uz"));
+                System.out.println();
+                System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "ru"));
+                System.out.println();
+                System.out.printf("%s = %s", df.format(number),  NumberToTextUtils.toText(number, "uk"));
+                System.out.println();
+                System.out.println("---------------");
+                System.out.println("Press the enter to continue...");
+                new Scanner(System.in).nextLine();
+
+                // eng asosiy xato hisoblangan sonlar
+                // 500008000000.85 Besh yuz milliard yuz sakkiz million butun yuzdan sakson besh
+                // 56789012        Ellik olti million yetti yuz sakson to'qqiz ming
+                // 50000007000 = Ellik milliard million yetti
+                // -933691099782589 = Minus to'qqiz yuz o'ttiz uch trillion olti yuz to'qson bir milliard yuz to'qson to'qqiz million yetti yuz sakson ikki ming besh yuz sakson to'qqiz
+                // -604563185080300 = Minus olti yuz to'rt trillion besh yuz oltmish uch milliard bir yuz sakson besh million sakson uch yuz
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Press the enter to continue...");
+                new Scanner(System.in).nextLine();
+            }
         }
     }
 }
--- src/main/java/uz/furorprogress/NumberToTextUtils.java
+++ src/main/java/uz/furorprogress/NumberToTextUtils.java
@@ -1,9 +1,7 @@
 package uz.furorprogress;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public final class NumberToTextUtils {
     private static final String UZ = "uz";
@@ -12,10 +10,23 @@ public final class NumberToTextUtils {
 
     private static final NumberLanguages YUZ_TO_TEXT = NumberLanguages.of("yuz", "юз", "сто");
     private static final NumberLanguages MING_TO_TEXT = NumberLanguages.of("ming", "минг", "тысяча");
-    private static final NumberLanguages MILLION_TO_TEXT = NumberLanguages.of("million", "миллион", "миллион");
+    private static final NumberLanguages MILLION_TO_TEXT = NumberLanguages.of("million", "миллион", "мильён");
     private static final NumberLanguages MILLIARD_TO_TEXT = NumberLanguages.of("milliard", "миллиард", "миллиард");
     private static final NumberLanguages TRILLION_TO_TEXT = NumberLanguages.of("trillion", "триллион", "триллион");
     private static final NumberLanguages MINUS_TEXT = NumberLanguages.of("minus ", "минус ", "минус ");
+    private static final NumberLanguages BUTUN_TEXT = NumberLanguages.of(" butun %sdan %s", " бутун %sдан %s", " целых %s из %s");
+    private static final NumberLanguages NOL_TEXT = NumberLanguages.of("nol", "нол", "Ноль");
+    private static final List<NumberLanguages> TEXT_OF_BIG_NUMBERS_LIST = new ArrayList<NumberLanguages>(){{
+        add(YUZ_TO_TEXT);
+        add(MING_TO_TEXT);
+        add(MILLION_TO_TEXT);
+        add(MILLIARD_TO_TEXT);
+        add(TRILLION_TO_TEXT);
+    }};
+
+    private static final List<NumberLanguages> TEXT_OF_BIG_NUMBERS_LIST_WITHOUT_HUNDRED = TEXT_OF_BIG_NUMBERS_LIST.stream()
+            .filter(numberLanguages -> !numberLanguages.equals(YUZ_TO_TEXT))
+            .collect(Collectors.toList());
 
     private static final Map<Integer, NumberLanguages> BIRLAR_XONASI = ofMap(
             1, NumberLanguages.of("bir", "бир", "один"),
@@ -29,7 +40,7 @@ public final class NumberToTextUtils {
             9, NumberLanguages.of("to'qqiz", "тўққиз", "девять")
     );
 
-    private static final Map<Integer, NumberLanguages> ONDAN_BIRDAN_TOQQIZGACHA_RAQAMLAR_XONASI = ofMap(
+    private static final Map<Integer, NumberLanguages> ONDAN_BIRDAN_ON_TOQQIZGACHA_RAQAMLAR_XONASI = ofMap(
             1, NumberLanguages.of("o'n bir", "ўн бир", "одиннадцать"),
             2, NumberLanguages.of("o'n ikki", "ўн икки", "двенадцать"),
             3, NumberLanguages.of("o'n uch", "ўн уч", "тринадцать"),
@@ -66,12 +77,27 @@ public final class NumberToTextUtils {
             this.ru=ru;
         }
 
-        public static NumberLanguages of(String uz, String uk, String ru) {
-            return new NumberLanguages(uz, uk, ru);
+        @Override
+        public boolean equals(Object obj) {
+            if (this==obj) {
+                return true;
+            }
+            if (obj==null) {
+                return false;
+            }
+            try {
+                if (obj instanceof NumberLanguages) {
+                    NumberLanguages other = (NumberLanguages)obj;
+                    return uz.equals(other.uz) || uk.equals(other.uk) || ru.equals(other.ru);
+                }
+            }catch (NullPointerException e){
+                return false;
+            }
+            return super.equals(obj);
         }
 
-        public static NumberLanguages ofEmptyString() {
-            return new NumberLanguages("", "", "");
+        public static NumberLanguages of(String uz, String uk, String ru) {
+            return new NumberLanguages(uz, uk, ru);
         }
 
         private String getUz() {
@@ -99,7 +125,43 @@ public final class NumberToTextUtils {
         }
     }
 
-    public static String toText(long number, String language) {
+
+    public static String toText(double number, String language) {
+        language = language==null || language.isEmpty() ? "uk" : language;
+        long first = (long) Math.floor(number);
+        double partOfWhole = number - first;
+        double summa = Math.round(partOfWhole*100);
+        long second = (long) summa;
+        second = second % 10 == 0 ? second/10 : second;
+        int size = (long) summa % 10 == 0 ? 1 : 2;
+        String text = "";
+        String firstText = toText(first, language);
+        if (first==0){
+            firstText = NOL_TEXT.getText(language);
+        }
+        if (second>0) {
+            String secondText = toText(second, language);
+
+            String butunText = size==1 ? ONLAR_XONASI.get(1).getText(language) : size==2 ? YUZ_TO_TEXT.getText(language) : "";
+
+            switch (language){
+                case RU: {
+                    text = (firstText + String.format(BUTUN_TEXT.getText(RU), secondText, butunText)).intern();
+                    break;
+                }
+                default: {
+                    text = (firstText + String.format(BUTUN_TEXT.getText(language), butunText, secondText)).intern();
+                    break;
+                }
+            }
+            String trimmed = text.intern().trim();
+            return !trimmed.isEmpty() ? (Character.toUpperCase(trimmed.charAt(0))+trimmed.substring(1)).intern() : "";
+        }
+        String trimmed = firstText.intern().trim();
+        return !trimmed.isEmpty() ? (Character.toUpperCase(trimmed.charAt(0))+trimmed.substring(1)).intern() : "";
+    }
+
+    private static String toText(long number, String language) {
         boolean isNegative = number < 0;
 
         StringBuilder text = new StringBuilder();
@@ -113,7 +175,6 @@ public final class NumberToTextUtils {
 
         long n = number;
         int onXonalikSonningIkkinchiRaqami = 0;
-        int ikkitaOldingiRaqami = 0;
 
         int i=0;
         while (n > 0) {
@@ -123,7 +184,10 @@ public final class NumberToTextUtils {
             switch (i) {
                 case 1: {
                     onXonalikSonningIkkinchiRaqami = digit;
-                    sonlarListi.add(BIRLAR_XONASI.getOrDefault(digit, NumberLanguages.ofEmptyString()).getText(language));
+                    NumberLanguages numberLanguages = BIRLAR_XONASI.get(digit);
+                    if (numberLanguages!=null) {
+                        sonlarListi.add(numberLanguages.getText(language));
+                    }
                     break;
                 }
 
@@ -134,13 +198,21 @@ public final class NumberToTextUtils {
                 case 2: {
 
                     if (digit==1 && onXonalikSonningIkkinchiRaqami!=0) {
-                        sonlarListi.remove(sonlarListi.size()-1);
-                        sonlarListi.add(ONDAN_BIRDAN_TOQQIZGACHA_RAQAMLAR_XONASI.getOrDefault(onXonalikSonningIkkinchiRaqami, NumberLanguages.ofEmptyString()).getText(language));
-                        break;
+                        if (!sonlarListi.isEmpty()) {
+                            sonlarListi.remove(sonlarListi.size()-1);
+                        }
+                        NumberLanguages numberLanguages = ONDAN_BIRDAN_ON_TOQQIZGACHA_RAQAMLAR_XONASI.get(onXonalikSonningIkkinchiRaqami);
+                        if (numberLanguages!=null) {
+                            sonlarListi.add(numberLanguages.getText(language));
+                            break;
+                        }
                     }
 
-                    sonlarListi.add(ONLAR_XONASI.getOrDefault(digit, NumberLanguages.ofEmptyString()).getText(language));
-                    break;
+                    NumberLanguages numberLanguages = ONLAR_XONASI.get(digit);
+                    if (numberLanguages!=null) {
+                        sonlarListi.add(numberLanguages.getText(language));
+                        break;
+                    }
                 }
 
                 case 15:
@@ -153,39 +225,48 @@ public final class NumberToTextUtils {
                 case 4:
                 case 3: {
                     if (i==3 || i==6 || i==9 || i==12 || i==15) {
-                        if (i!=3 && digit==0 && ikkitaOldingiRaqami==0) {
-                            sonlarListi.remove(sonlarListi.size()-3);
-                            break;
+                        if (digit!=0) {
+                            sonlarListi.add(YUZ_TO_TEXT.getText(language));
                         }
-                        if (digit==0 && ikkitaOldingiRaqami==0) {
-                            sonlarListi.remove(sonlarListi.size()-1);
-                            break;
-                        }
-                        sonlarListi.add(YUZ_TO_TEXT.getText(language));
                     }
                     if (i==4) {
+                        String str = sonlarListi.isEmpty() ? null : sonlarListi.get(sonlarListi.size() - 1);
+                        if (TEXT_OF_BIG_NUMBERS_LIST_WITHOUT_HUNDRED.contains(NumberLanguages.of(str, str, str))) {
+                            sonlarListi.remove(sonlarListi.size()-1);
+                        }
                         sonlarListi.add(MING_TO_TEXT.getText(language));
                         onXonalikSonningIkkinchiRaqami = digit;
-                        ikkitaOldingiRaqami = digit;
                     }
                     if (i==7) {
+                        String str = sonlarListi.get(sonlarListi.size() - 1);
+                        if (TEXT_OF_BIG_NUMBERS_LIST_WITHOUT_HUNDRED.contains(NumberLanguages.of(str, str, str))) {
+                            sonlarListi.remove(sonlarListi.size()-1);
+                        }
                         sonlarListi.add(MILLION_TO_TEXT.getText(language));
                         onXonalikSonningIkkinchiRaqami = digit;
-                        ikkitaOldingiRaqami = digit;
                     }
                     if (i==10) {
+                        String str = sonlarListi.get(sonlarListi.size() - 1);
+                        if (TEXT_OF_BIG_NUMBERS_LIST_WITHOUT_HUNDRED.contains(NumberLanguages.of(str, str, str))) {
+                            sonlarListi.remove(sonlarListi.size()-1);
+                        }
                         sonlarListi.add(MILLIARD_TO_TEXT.getText(language));
                         onXonalikSonningIkkinchiRaqami = digit;
-                        ikkitaOldingiRaqami = digit;
                     }
                     if (i==13) {
+                        String str = sonlarListi.get(sonlarListi.size() - 1);
+                        if (TEXT_OF_BIG_NUMBERS_LIST_WITHOUT_HUNDRED.contains(NumberLanguages.of(str, str, str))) {
+                            sonlarListi.remove(sonlarListi.size()-1);
+                        }
                         sonlarListi.add(TRILLION_TO_TEXT.getText(language));
                         onXonalikSonningIkkinchiRaqami = digit;
-                        ikkitaOldingiRaqami = digit;
                     }
 
-                    sonlarListi.add(BIRLAR_XONASI.getOrDefault(digit, NumberLanguages.ofEmptyString()).getText(language));
-                    break;
+                    NumberLanguages numberLanguages = BIRLAR_XONASI.get(digit);
+                    if (numberLanguages!=null) {
+                        sonlarListi.add(numberLanguages.getText(language));
+                        break;
+                    }
                 }
             }
 
@@ -202,8 +283,7 @@ public final class NumberToTextUtils {
             }
         }
 
-        String trimmed = text.toString().intern().trim();
-        return !trimmed.isEmpty() ? (Character.toUpperCase(trimmed.charAt(0))+trimmed.substring(1)).intern() : "";
+        return text.toString().trim();
     }
 
     private static <K, V> Map<K, V> ofMap(Object... keysAndValues) {
@@ -217,6 +297,7 @@ public final class NumberToTextUtils {
         for (int i = 0; i < keysAndValues.length; i++) {
             if (i % 2 == 0) {
                 try {
+                    // putting keys
                     objectObjectHashMap.put((K)keysAndValues[i], null);
                 }catch (Exception e) {
                     throw new RuntimeException("Key data type is mismatching", e);
@@ -224,11 +305,13 @@ public final class NumberToTextUtils {
                 continue;
             }
             try {
+                // putting values
                 objectObjectHashMap.put((K)keysAndValues[i-1], (V)keysAndValues[i]);
             }catch (Exception e) {
                 throw new RuntimeException("Key or value data type is mismatching", e);
             }
         }
+
         return objectObjectHashMap;
     }
 }
